7b03b4695759a525cdb3f852bc24e1e51da9bdbf
M.Furkan <byazade@yandex.com>
2019-09-16 20:07:10 +0300

    Create get_admins.py

---
+++ pyrobot/plugins/get_admins.py
@@ -0,0 +1,37 @@
+import pyrogram
+from pyrogram import Filters, Client
+from pyrobot import app, cmd
+
+
+@app.on_message(Filters.command(["get_admin"], cmd) & Filters.me)
+async def get_admins(client, message):
+      mentions = "Admin List: \n"
+      input_str = message.text[11:]
+      if not input_str:
+         input_str = message.chat.id
+      try:
+         async for x in client.iter_chat_members(chat_id=input_str, filter="administrators"):
+               if x.status == "creator":
+                   mentions += "\n 👑 [{}](tg://user?id={}) `{}`".format(x.user.first_name, x.user.id, x.user.id)
+               if x.status == "administrator":
+                   mentions += "\n ⚜ [{}](tg://user?id={}) `{}`".format(x.user.first_name, x.user.id, x.user.id)
+      except Exception as e:
+             mentions += " " + str(e) + "\n"
+      await message.edit(mentions)
+      
+@app.on_message(Filters.command(["men_admin"], cmd) & Filters.me)
+async def men_admins(client, message):
+      mentions = "Admin List: \n"
+      input_str = message.text[11:]
+      if not input_str:
+         input_str = message.chat.id
+      try:
+         async for x in client.iter_chat_members(chat_id=input_str, filter="administrators"):
+               if x.status == "creator":
+                   mentions += "\n 👑 [{}](tg://user?id={}) `{}`".format(x.user.first_name, x.user.id, x.user.id)
+               if x.status == "administrator":
+                   mentions += "\n ⚜ [{}](tg://user?id={}) `{}`".format(x.user.first_name, x.user.id, x.user.id)
+      except Exception as e:
+             mentions += " " + str(e) + "\n"
+      await message.reply(mentions)
+      await message.delete()
