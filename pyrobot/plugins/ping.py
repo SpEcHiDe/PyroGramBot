df64dcb9f70849dbb038fd9a9d5fa73c7e2feb3b
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-09-29 15:07:36 +0530

    Add alive command

--- pyrobot/plugins/ping.py
+++ pyrobot/plugins/ping.py
@@ -17,6 +17,16 @@ REPO = ("Userbot is available on GitHub:\n"
 # -- Constants End -- #
 
 
+@Client.on_message(Filters.command("alive", COMMAND_HAND_LER)  & Filters.me)
+async def help_me(client, message):
+    await message.edit(ALIVE)
+
+
+@Client.on_message(Filters.command("helpme", COMMAND_HAND_LER)  & Filters.me)
+async def help_me(client, message):
+    await message.edit(HELP)
+
+
 @Client.on_message(Filters.command("ping", COMMAND_HAND_LER)  & Filters.me)
 async def ping(client, message):
     start_t = datetime.now()
@@ -29,8 +39,3 @@ async def ping(client, message):
 @Client.on_message(Filters.command("repo", COMMAND_HAND_LER)  & Filters.me)
 async def repo(client, message):
     await message.edit(REPO)
-
-
-@Client.on_message(Filters.command("helpme", COMMAND_HAND_LER)  & Filters.me)
-async def help_me(client, message):
-    await message.edit(HELP)
