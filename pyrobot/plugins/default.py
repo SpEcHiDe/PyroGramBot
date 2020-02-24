800e309248f12306cd84a8b495aeec2649dfb45a
W4RR10R <46273006+CW4RR10R@users.noreply.github.com>
2020-02-24 10:59:59 +0530

    Update default.py

--- pyrobot/plugins/default.py
+++ pyrobot/plugins/default.py
@@ -13,51 +13,54 @@ from pyrobot import (
     LOGGER
 )
 
-
 @Client.on_message(Filters.command("load", COMMAND_HAND_LER)  & Filters.me)
 async def load_plugin(client, message):
     await message.edit("Processing ...")
-    if message.reply_to_message is not None:
-        down_loaded_plugin_name = await message.reply_to_message.download(
-            file_name="./plugins/"
-        )
-        if down_loaded_plugin_name is not None:
-            # LOGGER.info(down_loaded_plugin_name)
-            relative_path_for_dlpn = os.path.relpath(
-                down_loaded_plugin_name,
-                os.getcwd()
+    try:
+        if message.reply_to_message is not None:
+            down_loaded_plugin_name = await message.reply_to_message.download(
+                file_name="./plugins/"
             )
-            # LOGGER.info(relative_path_for_dlpn)
-            lded_count = 0
-            path = Path(relative_path_for_dlpn)
-            module_path = ".".join(
-                path.parent.parts + (path.stem,)
-            )
-            # LOGGER.info(module_path)
-            module = reload(import_module(module_path))
-            # https://git.io/JvlNL
-            for name in vars(module).keys():
-                # noinspection PyBroadException
-                try:
-                    handler, group = getattr(module, name).handler
-
-                    if isinstance(handler, Handler) and isinstance(group, int):
-                        client.add_handler(handler, group)
-
-                        LOGGER.info(
-                            '[{}] [LOAD] {}("{}") in group {} from "{}"'.format(
-                                client.session_name,
-                                type(handler).__name__,
-                                name,
-                                group,
-                                module_path
+            if down_loaded_plugin_name is not None:
+                # LOGGER.info(down_loaded_plugin_name)
+                relative_path_for_dlpn = os.path.relpath(
+                    down_loaded_plugin_name,
+                    os.getcwd()
+                )
+                # LOGGER.info(relative_path_for_dlpn)
+                lded_count = 0
+                path = Path(relative_path_for_dlpn)
+                module_path = ".".join(
+                    path.parent.parts + (path.stem,)
+                )
+                # LOGGER.info(module_path)
+                module = reload(import_module(module_path))
+                # https://git.io/JvlNL
+                for name in vars(module).keys():
+                    # noinspection PyBroadException
+                    try:
+                        handler, group = getattr(module, name).handler
+    
+                        if isinstance(handler, Handler) and isinstance(group, int):
+                            client.add_handler(handler, group)
+                            LOGGER.info(
+                                '[{}] [LOAD] {}("{}") in group {} from "{}"'.format(
+                                    client.session_name,
+                                    type(handler).__name__,
+                                    name,
+                                    group,
+                                    module_path
+                                )
                             )
-                        )
-
-                        lded_count += 1
-                except Exception as e:
-                    # LOGGER.info(str(e))
-                    pass
-            await message.edit(
-                f"installed {lded_count} commands / plugins"
-            )
+    
+                            lded_count += 1
+                    except Exception as e:
+                        # LOGGER.info(str(e))
+                        pass
+                await message.edit(
+                    f"installed {lded_count} commands / plugins"
+                )
+    except Exception as error:
+        await message.edit(
+             f"ERROR: `{error}`"
+        )
