cc90e3a3cf8a206b3e02041d12fc585c5ee079cd
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2020-09-06 13:05:52 +0530

    add filters
    testing to trigger updater 😅

--- pyrobot/plugins/tools/ping.py
+++ pyrobot/plugins/tools/ping.py
@@ -7,6 +7,7 @@ from pyrobot import (
     COMMAND_HAND_LER,
     IS_BOT
 )
+from pyrobot.helper_functions.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
 ALIVE = "ചത്തിട്ടില്ലാ..."
@@ -16,17 +17,17 @@ REPO = ("User / Bot is available on GitHub:\n"
 # -- Constants End -- #
 
 
-@Client.on_message(filters.command(["alive", "start"], COMMAND_HAND_LER))
+@Client.on_message(filters.command(["alive", "start"], COMMAND_HAND_LER) & f_onw_fliter)
 async def check_alive(_, message):
     await message.reply_text(ALIVE)
 
 
-@Client.on_message(filters.command("help", COMMAND_HAND_LER))
+@Client.on_message(filters.command("help", COMMAND_HAND_LER) & f_onw_fliter)
 async def help_me(_, message):
     await message.reply_sticker(HELP)
 
 
-@Client.on_message(filters.command("ping", COMMAND_HAND_LER))
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
 async def ping(_, message):
     start_t = time.time()
     rm = await message.reply_text("...")
@@ -35,6 +36,6 @@ async def ping(_, message):
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
 
-@Client.on_message(filters.command("repo", COMMAND_HAND_LER))
+@Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
 async def repo(_, message):
     await message.reply_text(REPO)
