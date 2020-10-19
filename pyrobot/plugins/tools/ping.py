01f6278381caa2953d5e9026cbfe84cd60497692
iiiiii1wepfj <itaykotik@gmail.com>
2020-10-19 15:22:57 +0300

    add emoji to ping

--- pyrobot/plugins/tools/ping.py
+++ pyrobot/plugins/tools/ping.py
@@ -32,10 +32,10 @@ async def help_me(_, message):
 @Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
 async def ping(_, message):
     start_t = time.time()
-    rm = await message.reply_text("...")
+    rm = await message.reply_text("pong...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
+    await rm.edit(f"Pong!🏓\n{time_taken_s:.3f} ms")
 
 
 @Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
