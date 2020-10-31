fa4cde8bcea9643779dc98f6113dcd37744efcb7
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2020-10-31 11:26:56 +0530

    minor fixes

--- pyrobot/helper_functions/scheme.py
+++ pyrobot/helper_functions/scheme.py
@@ -3,8 +3,9 @@ from https://github.com/Lonami/TelethonianBotExt/blob/master/layer.py
 modified to suit the needs of this application"""
 
 import asyncio
-import io
-import sys
+import aiohttp
+import os
+from io import BytesIO
 from pyrogram import filters
 from pyrobot import (
     LAYER_FEED_CHAT,
@@ -14,49 +15,25 @@ from pyrobot import (
 from pyrobot.pyrobot import PyroBot
 
 
-async def fetch():
-    BASE_URI = b"raw.githubusercontent.com"
-    URI = b"telegramdesktop/tdesktop/dev/Telegram/Resources/tl/api.tl"
-    # We could use aiohttp but this is more fun
-    rd, wr = await asyncio.open_connection(BASE_URI, 443, ssl=True)
-
-    # Send HTTP request
-    wr.write(
-        b"GET /" + URI + b" HTTP/1.1\r\n"
-        b"Host: " + BASE_URI + "\r\n"
-        b"\r\n"
-    )
-    await wr.drain()
-
-    # Get response headers
-    headers = await rd.readuntil(b"\r\n\r\n")
-    if headers[-4:] != b"\r\n\r\n":
-        raise ConnectionError("Connection closed")
-
-    # Ensure it"s OK
-    if not headers.startswith(b"HTTP/1.1 200 OK"):
-        raise ValueError("Bad status code: {}".format(headers[:headers.index(b"\r\n")]))
-
-    # Figure out Content-Length to read
-    index = headers.index(b"Content-Length:") + 16
-    length = int(headers[index:headers.index(b"\r", index)])
-    result = await rd.readexactly(length)
-
-    # Properly close the writer
-    wr.close()
-    if sys.version_info >= (3, 7):
-        await wr.wait_closed()
-
-    return result
+async def fetch(scheme_url: str):
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(scheme_url)
+        return await response.text()
 
 
 async def check_feed(client: PyroBot):
-    last_hash = hash(await fetch())
+    layer_uri = (
+        "https://"
+        "github.com"
+        "/telegramdesktop/tdesktop/raw/dev/Telegram/Resources/tl/"
+        "api.tl"
+    )
+    last_hash = hash(await fetch(layer_uri))
     while True:
-        contents = await fetch()
+        contents = await fetch(layer_uri)
         if hash(contents) != last_hash:
-            file = io.BytesIO(contents)
-            file.name = "scheme.tl"
+            file = BytesIO(contents)
+            file.name = os.path.basename(layer_uri)
             message = await client.send_document(
                 LAYER_FEED_CHAT,
                 file,
