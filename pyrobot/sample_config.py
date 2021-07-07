46b68eaa9e6485645fca5af8247a28db3f53b12f
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-07-07 23:41:10 +0530

    t, ftm, op, noU

--- pyrobot/sample_config.py
+++ pyrobot/sample_config.py
@@ -1,4 +1,27 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+#  GetSongsBot
+#  Copyright (C) 2021 The Authors
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
+from dotenv import load_dotenv
+
+
+# apparently, no error appears even if the path does not exists
+load_dotenv("config.env")
 
 
 class Config:
