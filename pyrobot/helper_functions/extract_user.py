db3c7e712b690befdf8c608a8f78889e6820ec7d
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-08-04 10:36:17 +0530

    try

--- pyrobot/helper_functions/extract_user.py
+++ pyrobot/helper_functions/extract_user.py
@@ -14,20 +14,15 @@ def extract_user(message: Message) -> (int, str):
         user_first_name = message.reply_to_message.from_user.first_name
 
     elif len(message.command) > 1:
-        if len(message.entities) > 1:
+        if (
+            len(message.entities) > 1 and
+            message.entities[1].type == "text_mention"
+        ):
             # 0: is the command used
             # 1: should be the user specified
             required_entity = message.entities[1]
-            if required_entity.type == "text_mention":
-                user_id = required_entity.user.id
-                user_first_name = required_entity.user.first_name
-            elif required_entity.type in ["mention", "phone_number"]:
-                user_id = message.text[
-                    required_entity.offset:
-                    required_entity.offset + required_entity.length
-                ]
-                # don't want to make a request -_-
-                user_first_name = user_id
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
         else:
             user_id = message.command[1]
             # don't want to make a request -_-
