c2c2cac1452a0aa67b69ed02e93186b719cfff30
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-08-26 20:21:22 +0530

    fix IndentationError
    regression was introduced in d4599af6bab5fd2d6fbccda6e6a589a35561df2f

--- pyrobot/plugins/admemes/whois.py
+++ pyrobot/plugins/admemes/whois.py
@@ -29,58 +29,59 @@ async def who_is(client, message):
         return
     if from_user is None:
         await status_message.edit("no valid user_id / message specified")
-    message_out_str = ""
-        message_out_str += "<b>Name:</b> "
-        message_out_str += f"<a href='tg://user?id={from_user.id}'>"
-        message_out_str += from_user.first_name or ""
-        message_out_str += "</a>\n"
-        last_name = from_user.last_name or ""
-        message_out_str += f"<b>Suffix:</b> {last_name}\n"
-        username = from_user.username or ""
-        message_out_str += f"<b>Username:</b> @{username}\n"
-        message_out_str += f"<b>User ID:</b> <code>{from_user.id}</code>\n"
-        dc_id = from_user.dc_id or "[User Doesnt Have A Valid DP 👀]"
-        message_out_str += f"<b>DC ID:</b> <code>{dc_id}</code>\n"
-        message_out_str += f"<b>User Link:</b> {from_user.mention}\n" if from_user.username else ""
-        message_out_str += f"<b>Is Deleted:</b> True\n" if from_user.is_deleted else ""
-        message_out_str += f"<b>Is Verified:</b> True" if from_user.is_verified else ""
-        message_out_str += f"<b>Is Scam:</b> True" if from_user.is_scam else ""
-        message_out_str += f"<b>Last Seen:</b> <code>{last_online(from_user)}</code>\n\n"
-        if message.chat.type in (("supergroup", "channel")):
-            try:
-                chat_member_p = await message.chat.get_member(from_user.id)
-                joined_date = datetime.fromtimestamp(
-                    chat_member_p.joined_date or time.time()
-                ).strftime("%Y.%m.%d %H:%M:%S")
-                message_out_str += (
-                    "<b>Joined on:</b> <code>"
-                    f"{joined_date}"
-                    "</code>\n"
-                )
-            except UserNotParticipant:
-                pass
-        chat_photo = from_user.photo
-        if chat_photo:
-            local_user_photo = await client.download_media(
-                message=chat_photo.big_file_id
+        return
+    
+    first_name = from_user.first_name or ""
+    last_name = from_user.last_name or ""
+    username = from_user.username or ""
+    
+    message_out_str = (
+        "<b>Name:</b> "
+        f"<a href='tg://user?id={from_user.id}'>{first_name}</a>\n"
+        f"<b>Suffix:</b> {last_name}\n"
+        f"<b>Username:</b> @{username}\n"
+        f"<b>User ID:</b> <code>{from_user.id}</code>\n"
+        f"<b>User Link:</b> {from_user.mention}\n" if from_user.username else ""
+        f"<b>Is Deleted:</b> True\n" if from_user.is_deleted else ""
+        f"<b>Is Verified:</b> True" if from_user.is_verified else ""
+        f"<b>Is Scam:</b> True" if from_user.is_scam else ""
+        # f"<b>Is Fake:</b> True" if from_user.is_fake else ""
+        f"<b>Last Seen:</b> <code>{last_online(from_user)}</code>\n\n"
+    )
+
+    if message.chat.type in ["supergroup", "channel"]:
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(
+                chat_member_p.joined_date or time.time()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>Joined on:</b> <code>"
+                f"{joined_date}"
+                "</code>\n"
             )
-            await message.reply_photo(
-                photo=local_user_photo,
-                quote=True,
-                caption=message_out_str,
-                parse_mode="html",
-                # ttl_seconds=,
-                disable_notification=True
-            )
-            os.remove(local_user_photo)
-        else:
-            await message.reply_text(
-                text=message_out_str,
-                quote=True,
-                parse_mode="html",
-                disable_notification=True
-            )
-        await status_message.delete()
+        except UserNotParticipant:
+            pass
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(
+            message=chat_photo.big_file_id
+        )
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            caption=message_out_str,
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
+    else:
+        await message.reply_text(
+            text=message_out_str,
+            quote=True,
+            disable_notification=True
+        )
+    await status_message.delete()
+
 
 def last_online(from_user):
     time = ""
