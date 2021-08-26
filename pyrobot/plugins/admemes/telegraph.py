8c982bd7dd98b26d9c8e59c1aa6703e4494131aa
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-08-26 20:46:14 +0530

    modify to suit my needs

--- pyrobot/plugins/admemes/telegraph.py
+++ pyrobot/plugins/admemes/telegraph.py
@@ -1,47 +1,46 @@
 import os
-
-from pyrogram import filters
+import shutil
+from pyrogram import Client, filters
 from telegraph import upload_file
+from pyrobot import TMP_DOWNLOAD_DIRECTORY
+from pyrobot.helper_functions.cust_p_filters import sudo_filter
+from pyrobot.helper_functions.get_file_id import get_file_id
 
-from pyrogram import Client
 
-
-@Client.on_message(filters.command("telegraph"))
+@Client.on_message(
+    filters.command("telegraph") &
+    sudo_filter
+)
 async def telegraph(client, message):
     replied = message.reply_to_message
     if not replied:
-        await message.reply("Reply to a supported media file")
+        await message.reply_text("Reply to a supported media file")
         return
-    if not (
-        (replied.photo and replied.photo.file_size <= 5242880)
-        or (replied.animation and replied.animation.file_size <= 5242880)
-        or (
-            replied.video
-            and replied.video.file_name.endswith(".mp4")
-            and replied.video.file_size <= 5242880
-        )
-        or (
-            replied.document
-            and replied.document.file_name.endswith(
-                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
-            )
-            and replied.document.file_size <= 5242880
-        )
-    ):
-        await message.reply("Not supported!")
+    file_info = get_file_id(replied)
+    if not file_info:
+        await message.reply_text("Not supported!")
         return
-    download_location = await client.download_media(
-        message=message.reply_to_message,
-        file_name="root/downloads/",
+    _t = os.path.join(
+        TMP_DOWNLOAD_DIRECTORY,
+        str(replied.message_id)
+    )
+    if not os.path.isdir(_t):
+        os.makedirs(_t)
+    _t += "/"
+    download_location = await replied.download(
+        _t
     )
     try:
         response = upload_file(download_location)
     except Exception as document:
-        await message.reply(message, text=document)
+        await message.reply_text(message, text=document)
     else:
         await message.reply(
-            f"**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}**",
-            disable_web_page_preview=True,
+            f"https://telegra.ph{response[0]}",
+            disable_web_page_preview=True
         )
     finally:
-        os.remove(download_location)
+        shutil.rmtree(
+            _t,
+            ignore_errors=True
+        )
