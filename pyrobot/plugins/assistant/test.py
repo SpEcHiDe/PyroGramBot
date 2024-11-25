ee88b8d87dfae6fc5d19c7122b2070ec5c87d28a
shriMADhav U k <hetzner@shrimadhavuk.me>
2024-11-25 16:59:19 +0100

    Adding test code

--- pyrobot/plugins/assistant/test.py
+++ pyrobot/plugins/assistant/test.py
@@ -16,3 +16,12 @@ from pyrogram import Client, filters
 # async def _(_, m):
 #     print(m)
 #     await m.reply(str(m.media))
+
+# @Client.on_poll(
+# )
+# async def _(_, p):
+#     print(p)
+
+# @Client.on_chat_member_updated()
+# async def _(c, u):
+#  await c.send_message(-1001220993104, str(u._raw), message_thread_id=1378275)
